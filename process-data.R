8488b1c35b4c242bdf6f9b8f0e375c1048fb3e07
jaredwins99 <jaredwins99@gmail.com>
2024-11-26 11:29:23 -0500

    refactored code for cleaning, added wave 10 data
    - created cleaning pipeline
    - added wave10 data
    - removed output file
    - added folders for better data organization

--- process-data.R
+++ process-data.R
@@ -1,126 +1,37 @@
+library(tidyr)
 library(dplyr)
 library(forcats)
 library(haven)
 library(ggplot2)
+library(stringr)
 
-setwd("C:/Users/jared/Desktop/Jared/social-ties")
+setwd("C:/Users/Jared/Desktop/Columbia 2024 (3rd)/Gelman Lab/social-ties")
 
-RAW_DATA_FILENAME <- 'dat/RAW.rds'
+# ----- LOAD DATA -----
 
-RAW_DATA_FILENAME2 <- 'wave2/dat/2024-10-09-week-37-group-1-wave-10.sav'
+waves <- c(6,10)
+raw_data_filenames <- c()
+for (wave in waves) {
+  raw_data_filenames <- c(raw_data_filenames, paste0('dat/wave', wave, '/raw_wave_', wave, '.rds'))
+}
+dfs <- lapply(raw_data_filenames, readRDS)
 
-# LOAD DATA
-df <- readRDS(RAW_DATA_FILENAME)
-
-df2 <- read_sav(RAW_DATA_FILENAME2)
-
-View(df2)
-
-glimpse(df2)
-sink("output.txt")
-print(colnames(df2))
-sink()
-
-df %>% count(pid7)
-
-df2 %>% count(pid7)
-
-ggplot(df, aes(pid7)) + geom_bar()
-
-glimpse(df)
-
-ggplot() + 
-  geom_bar(data=df, aes(age4), fill='lightblue', alpha=.5) + 
-  geom_bar(data=df2, aes(age4), fill='blue', alpha=.2) + 
-  theme_minimal()
+# df %>% count(pid7)
+# df2 %>% count(pid7)
+# ggplot(df, aes(pid7)) + geom_bar()
+# ggplot() + 
+#   geom_bar(data=df, aes(age4), fill='lightblue', alpha=.5) + 
+#   geom_bar(data=df2, aes(age4), fill='blue', alpha=.2) + 
+#   theme_minimal()
 
 # ----- CLEAN/PROCESS VARIABLES -----
 
-# PARTY ID (pid3)
-# collapse "Other" to "Independent"
-df$pid3[df$pid3 == "Other"] <- "Independent"
-# throw out people who responded "Not sure" to `pid7`
-df <- df[df$pid7 != "Not sure",]
-# relevel the factor
-df$pid3 <- factor(df$pid3)
-df$pid3 <- fct_relevel(df$pid3, "Democrat", "Independent", "Republican")
+print_shape <- function(df) {
+  cat("Rows:", nrow(df), "| Columns:", ncol(df), "\n")
+  df
+}
 
-# REMOVE MISSING FRIEND_GROUP_PID3
-# (there's only 8 people who didn't answer)
-df <- df[!is.na(df$friend_group_pid3),]
-
-# RENAME FRIEND_GROUP_CLASS FACTOR LEVELS
-df$friend_group_class <- fct_recode(df$friend_group_class,
-  "Friends much wealthier" = "Much wealthier",
-  "Friends slightly wealthier" = "Slightly wealthier",
-  "Friends the same" = "The same",
-  "Friends slightly poorer" = "Slightly poorer",
-  "Friends much poorer" = "Much poorer"
-)
-
-# RENAME "urbancity" TO "urbanicity"
-df <- df %>% rename(urbanicity=urbancity)
-
-# IMPUTE NA URBANICITY RESPONSE
-# there's one person who didn't respond. they're in a zip code
-# with urban population: 37,974 and rural population: 6,230
-# (from https://www.city-data.com/zips/46360.html)
-# so just mark them as urban
-df$urbanicity[is.na(df$urbanicity)] <- "City"
-
-# CHANGE "Other" RESPONSES TO URBANICITY TO NA
-# this way we don't throw out the rows outright but
-# our plotting functions know to ignore them since there are so few.
-df[df$urbanicity == "Other" & !is.na(df$urbanicity), "urbanicity"] <- NA
-
-# ----- CREATE NEW VARIABLES -----
-
-# FRIEND_GROUP_PID5
-# This variable separates out those who don't have any friends of the opposing
-# party from those who do. Essentially we break up the "Mostly D" and "Mostly R"
-# categories based on respondents' answers to questions about whether they have
-# any friends of the opposing party.
-df$friend_group_pid5 <- df$friend_group_pid3
-df$friend_group_pid5 <- factor(df$friend_group_pid5, levels=c(
-  "All Democrats",
-  "Mostly Democrats",
-  "About evenly split",
-  "Mostly Republicans",
-  "All Republicans",
-  "Not sure"
-))
-# among those who are friends with "Mostly Democrats", change the covariate
-# value to "All Democrats" if `any_friends_republicans` is false.
-all_d <- df$friend_group_pid5 == "Mostly Democrats" & !(df$any_friends_republicans == "Yes")
-all_d[is.na(all_d)] <- FALSE
-df$friend_group_pid5[all_d] <- "All Democrats"
-all_r <- df$friend_group_pid5 == "Mostly Republicans" & !(df$any_friends_democrats == "Yes")
-all_r[is.na(all_r)] <- FALSE
-df$friend_group_pid5[all_r] <- "All Republicans"
-
-# FRIEND_GROUP_PID_NUMERIC
-# -2, -1, 0, 1, 2
-# For "All Democrats", ..., "All Republicans". Where we collapse "Not sure"
-# respondents to "About evenly split".
-df <- df %>% mutate(
-  friend_group_pid_numeric = ifelse(friend_group_pid5 == "Not sure", 0, as.numeric(friend_group_pid5) - 3),
-)
-
-# COLLAPSED_PID
-# Collapse people who "lean Dem" or "lean Rep" into their respective parties
-df$collapsed_pid <- "Independent/Not sure"
-lean_dem <- as.numeric(df$pid7) < 4
-lean_rep <- as.numeric(df$pid7) > 4 & (df$pid7 != "Not sure")
-df$collapsed_pid[lean_dem] <- "Democrat"
-df$collapsed_pid[lean_rep] <- "Republican"
-
-df$collapsed_pid <- factor(df$collapsed_pid, levels=c(
-  "Democrat",
-  "Independent/Not sure",
-  "Republican"
-))
-
-# FRIEND_GROUP_COPARTISANSHIP
+# Create mappings for Democrats and Republicans
 copartisanship_levels <- c(
   "All co-partisan",
   "Mostly co-partisan",
@@ -132,75 +43,123 @@ copartisanship_levels <- c(
 friend_group_partisan_levels <- head(levels(df$friend_group_pid5), -1)
 dem_levels <- c(friend_group_partisan_levels, "Not sure")
 rep_levels <- c(rev(friend_group_partisan_levels), "Not sure")
-# Create mappings for Democrats and Republicans
 dem_map <- setNames(copartisanship_levels, dem_levels)
 rep_map <- setNames(copartisanship_levels, rep_levels)
 
-# Create the new variable
-df$friend_group_copartisanship <- ifelse(
-  df$collapsed_pid == "Independent/Not sure", NA,
-       ifelse(df$collapsed_pid == "Democrat",
-              dem_map[as.character(df$friend_group_pid5)],
-              rep_map[as.character(df$friend_group_pid5)])
-)
+clean <- function(df) {
+  df %>%
+    rename(urbanicity = urbancity) %>% # Rename column urbancity to urbanicity
+    mutate(pid3 = factor(if_else(pid3 == "Other", "Independent", pid3)) %>%  # PARTY ID (pid3): Collapse "Other" to "Independent"
+             fct_relevel("Democrat", "Independent", "Republican")) %>%
+    # IMPUTE NA URBANICITY RESPONSE
+    # one person didn't respond re urbanicity, but they're in a zip code with an urban population
+    # 37,974 and rural population: 6,230 (from https://www.city-data.com/zips/46360.html)
+    replace_na(list(urbanicity = "City")) %>% 
+    # CHANGE "Other" RESPONSES IN URBANICITY TO NA
+    # allows plotting functions to ignore the small number of (~8) "Other" responses
+    mutate(urbanicity = if_else(urbanicity == "Other", NA, urbanicity)) %>%
+    print_shape() %>%
+    filter(pid7 != "Not sure") %>% # throw out people who responded "Not sure" to `pid7`
+    print_shape() %>%
+    drop_na(friend_group_pid3) %>%
+    print_shape() %>%
+    # FRIEND_GROUP_PID5
+    # This variable separates out those who don't have any friends of the opposing
+    # party from those who do. Essentially we break up the "Mostly D" and "Mostly R"
+    # categories based on respondents' answers to questions about whether they have
+    # any friends of the opposing party.
+    # among those who are friends with "Mostly Democrats", change the covariate
+    # value to "All Democrats" if `any_friends_republicans` is false.
+    mutate(friend_group_pid5 = case_when(replace_na(friend_group_pid3 == "Mostly Democrats" & !(any_friends_republicans == "Yes"), FALSE) ~ "All Democrats",
+                                         replace_na(friend_group_pid3 == "Mostly Republicans" & !(any_friends_democrats == "Yes"), FALSE) ~ "All Republicans",
+                                         TRUE ~ friend_group_pid3) %>% 
+             factor(levels = c("All Democrats","Mostly Democrats","About evenly split","Mostly Republicans","All Republicans","Not sure")),
+           # FRIEND_GROUP_PID_NUMERIC
+           # -2, -1, 0, 1, 2
+           # For "All Democrats", ..., "All Republicans". Where we collapse "Not sure"
+           # respondents to "About evenly split".
+           friend_group_pid_numeric = if_else(friend_group_pid5 == "Not sure", 
+                                              0, 
+                                              as.numeric(friend_group_pid5) - 3),
+           collapsed_pid = case_when(
+             as.numeric(pid7) < 4 ~ "Democrat",
+             as.numeric(pid7) > 4 & pid7 != "Not sure" ~ "Republican",
+             TRUE ~ "Independent/Not sure"
+             ) %>% fct_relevel("Democrat", "Independent/Not sure", "Republican"),
+           friend_group_copartisanship = if_else(collapsed_pid == "Independent/Not sure", 
+                                                 NA, 
+                                                 if_else(collapsed_pid == "Democrat",
+                                                         dem_map[as.character(friend_group_pid5)],
+                                                         rep_map[as.character(friend_group_pid5)])) %>% 
+             fct_relevel(copartisanship_levels),
+           friend_group_copartisanship_numeric = if_else(friend_group_copartisanship == "Not sure", 
+                                                         0, 
+                                                         as.numeric(friend_group_copartisanship) - 3))
+}
 
-# Convert the new variable to a factor with the specified levels
-df$friend_group_copartisanship <- factor(df$friend_group_copartisanship, 
-                                         levels = copartisanship_levels)
+clean_wave_6 <- function(df) {
+  df %>%
+    mutate(friend_group_class = friend_group_class %>% 
+             fct_recode("Friends much wealthier" = "Much wealthier",
+                        "Friends slightly wealthier" = "Slightly wealthier",
+                        "Friends the same" = "The same",
+                        "Friends slightly poorer" = "Slightly poorer",
+                        "Friends much poorer" = "Much poorer"))
+}
 
-# FRIEND_GROUP_COPARTISANSHIP_NUMERIC
-# -2, -1, 0, 1, 2
-# For "All Copartisan", ..., "All Counterpartisan". Where we collapse "Not sure"
-# respondents to "About evenly split".
-df <- df %>% mutate(
-  friend_group_copartisanship_numeric = ifelse(
-    friend_group_copartisanship == "Not sure", 0, as.numeric(friend_group_copartisanship) - 3
-    ),
-)
+clean(dfs[[1]])
 
-# CROSS_PARTY_OPENNESS
-# Define "openness to vote cross-party" based on responses to
-# whether Democrats would 1) consider voting for Trump, or 2) consider voting
-# for any Republican at all over Biden. Similar for Republicans voting for Biden
-# or voting for any Democrat at all over Trump.
-openness <- df %>% mutate(
-    consider_other_candidate = ifelse(
-      collapsed_pid == "Independent/Not sure",
-      NA,
-      ifelse(
-        collapsed_pid == "Democrat",
-        consider_trump,
-        consider_biden
-      )
-    ),
-    consider_other_party_candidate = ifelse(
-      collapsed_pid == "Independent/Not sure",
-      NA,
-      ifelse(
-        collapsed_pid == "Democrat",
-        consider_any_republican,
-        consider_any_democrat
-      )
-    )
-  ) %>%
-      mutate(
-        openness = case_when(
-          is.na(consider_other_candidate) | is.na(consider_other_party_candidate) ~ NA,
-          as.numeric(consider_other_candidate) == 1 ~ "Very open",
-          consider_other_party_candidate == 1 ~ "Somewhat open",
-          .default="Not open"
-        )
-      ) %>%
-      pull(openness)
+processed_dfs <- lapply(dfs, clean)
+processed_df1 <- clean_wave_6(processed_dfs[[1]])
+processed_df2 <- processed_dfs[[2]]
 
-df$cross_party_openness <- factor(
-  openness,
-  levels=c("Not open", "Somewhat open", "Very open"),
-  ordered=TRUE
-  )
+processed_df2 %>% dplyr::select(contains('friend'))
+
+# # ----- CREATE NEW VARIABLES -----
+# 
+# # CROSS_PARTY_OPENNESS
+# # Define "openness to vote cross-party" based on responses to
+# # whether Democrats would 1) consider voting for Trump, or 2) consider voting
+# # for any Republican at all over Biden. Similar for Republicans voting for Biden
+# # or voting for any Democrat at all over Trump.
+# openness <- df %>% mutate(
+#     consider_other_candidate = ifelse(
+#       collapsed_pid == "Independent/Not sure",
+#       NA,
+#       ifelse(
+#         collapsed_pid == "Democrat",
+#         consider_trump,
+#         consider_biden
+#       )
+#     ),
+#     consider_other_party_candidate = ifelse(
+#       collapsed_pid == "Independent/Not sure",
+#       NA,
+#       ifelse(
+#         collapsed_pid == "Democrat",
+#         consider_any_republican,
+#         consider_any_democrat
+#       )
+#     )
+#   ) %>%
+#       mutate(
+#         openness = case_when(
+#           is.na(consider_other_candidate) | is.na(consider_other_party_candidate) ~ NA,
+#           as.numeric(consider_other_candidate) == 1 ~ "Very open",
+#           consider_other_party_candidate == 1 ~ "Somewhat open",
+#           .default="Not open"
+#         )
+#       ) %>%
+#       pull(openness)
+# 
+# df$cross_party_openness <- factor(
+#   openness,
+#   levels=c("Not open", "Somewhat open", "Very open"),
+#   ordered=TRUE
+#   )
 
 # COUNTY_LEANING
-zip_to_county_raw <- read.csv('dat/zip_to_county.csv')
+zip_to_county <- read.csv('dat/geographic/zip_to_county.csv')
 
 zip_to_char <- function(zip) {
   zip_char <- as.character(zip)
@@ -211,17 +170,17 @@ zip_to_char <- function(zip) {
   )
 }
 
-df <- df %>%
+processed_df1 <- processed_df1 %>%
   mutate(
-    zipcode = zip_to_char(zipcode),
+    #zipcode = zip_to_char(zipcode),
     state_lower = tolower(inputstate)
-    ) %>%
+  ) %>%
   left_join(zip_to_county %>% dplyr::select(zipcode, state_lower, county), by=c('state_lower'='state_lower', 'zipcode'='zipcode'))
 
-
 # ---- SAVE DF ----
-processed <- df
-saveRDS(processed, 'dat/processed.rds')
+
+saveRDS(processed_df1, 'dat/wave6/processed_wave_6.rds')
+saveRDS(processed_df2, 'dat/wave10/processed_wave_10.rds')
 
 # ---- OTHER DATA SOURCES ----
 
